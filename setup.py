5458ed9aa839a754f684a6a1b3d1c8be0d55b987
Thomas Ramsey <rams0113@flinders.edu.au>
2020-05-17 13:33:32 +0930

    working file

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.LLAW33012020S1P04',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.LLAW33012020S1P04\n\nA docassemble extension.\n\n## Author\n\nThomas Ramsey, rams0113@flinders.edu.au\n\n',
+      long_description='# docassemble.LLAW33012020S1P04\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nThomas Ramsey, rams0113@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Thomas Ramsey',
       author_email='rams0113@flinders.edu.au',
